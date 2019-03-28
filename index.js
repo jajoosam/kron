5dbdda160d63c57c46682718d8a6ea5e8d35ce5d
Samarth Jajoo <jajoosam@Samarths-MacBook-Air.local>
2019-03-28 19:20:16 +0530

    Apply gitignore, add active count + 🤷 emoji

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require('express')
+const express = require('express');
+var mustacheExpress = require('mustache-express');
 const axios = require ('axios');
 const app = express()
 const port = 3000
 
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+var active = 0;
+
+
+
 app.get('/', (req, res) => {
 	if(req.query.time && req.query.url){
+		active+=1;
 		setTimeout(function(){
 		axios.get(decodeURIComponent(req.query.url))
 		  .then(function (response) {
-		    console.log("OK");
+				console.log("OK");
+				active-=1;
 		  })
 		  .catch(function (error) {
-		    console.log("ERROR");
+				console.log("ERROR");
+				active-=1;
 		  });
 		}, parseInt(req.query.time)*1000);
 		return res.send(`OK`);
 	}
 	else{
-		return res.sendFile('index.html' , { root : __dirname});
+		return res.render("index", {current: active});
 	}
 });
 
 app.get('*', function(req, res){
-  res.send(`¯\_(ツ)_/¯ - what are you even trying to do?`, 404);
+  res.send(`🤷 - what are you even trying to do?`, 404);
 });
 
 

7c64458caf3500fe9fddb6fa2535833cae3272cd
Samarth Jajoo <jajoosam@gmail.com>
2019-08-24 15:26:30 +0530

    Changing stuff up

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
-const express = require('express');
-var mustacheExpress = require('mustache-express');
-const axios = require ('axios');
-const isbot = require('isbot');
-const app = express()
-const port = 3000
+const express = require("express");
+var mustacheExpress = require("mustache-express");
+const axios = require("axios");
+const isbot = require("isbot");
+const nanoID = require("nanoid");
+const app = express();
+const port = 3000;
 
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
 
 var active = 0;
 
-app.get('/', (req, res) => {
-	if(req.query.time && req.query.url && !isbot(req.headers["user-agent"])){
-		active+=1;
-		setTimeout(function(){
-		axios.get(decodeURIComponent(req.query.url))
-		  .then(function (response) {
-				console.log("OK");
-				active-=1;
-		  })
-		  .catch(function (error) {
-				console.log("ERROR");
-				active-=1;
-		  });
-		}, parseInt(req.query.time)*1000);
-		return res.send(`OK`);
-	}
-	else{
-		return res.render("index", {current: active});
-	}
+let toCall = {};
+
+let IDs = {};
+
+app.get("/", (req, res) => {
+  if (
+    (req.query.time || req.query.date) &&
+    req.query.url &&
+    !isbot(req.headers["user-agent"])
+  ) {
+    const kronID = nanoID();
+    let date = "";
+    if (req.query.date) {
+      date = new Date(req.query.date).toGMTString();
+      if (date === "Invalid Date") {
+        return res.send({
+          error: true,
+          message: "You provided an invalid date 😕"
+        });
+      }
+    } else if (!isNaN(parseInt(req.query.time))) {
+      date = new Date();
+      date.setSeconds(date.getSeconds() + parseInt(req.query.time));
+      date = date.toGMTString();
+    } else {
+      return res.send({
+        error: true,
+        message: "You provided an invalid date 😕"
+      });
+    }
+    IDs[kronID] = { date: date, url: req.query.url };
+    if (toCall.hasOwnProperty(date)) {
+      toCall[date].push(kronID);
+    } else {
+      toCall[date] = [kronID];
+    }
+    console.log(toCall);
+    return res.send({
+      error: false,
+      message: kronID
+    });
+  } else {
+    return res.render("index", { current: active });
+  }
 });
 
-app.get('*', function(req, res){
-  res.send(`🤷 - what are you even trying to do?`, 404);
+const go = () => {
+  let date = new Date().toGMTString();
+  if (toCall.hasOwnProperty(date)) {
+    toCall[date].forEach(id => {
+      axios.get(IDs[id].url);
+      console.log(`✅ Completed ${id}`);
+      delete toCall[date];
+    });
+  }
+};
+
+setInterval(go, 1000);
+
+app.get("/delete", function(req, res) {
+  if (req.query.id) {
+    let kron = IDs[req.query.id];
+    let date = toCall[kron.date];
+    date = date.filter(e => e != req.query.id);
+    toCall[kron.date] = date;
+    console.log(`🗑️ Deleted ${req.query.id}`);
+    return res.send({
+      error: false,
+      message: "Successfully Deleted 🗑️"
+    });
+  }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("*", function(req, res) {
+  res.status(404).send(`🤷 - what are you even trying to do?`);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
